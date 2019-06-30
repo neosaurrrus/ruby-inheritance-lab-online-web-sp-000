72a9d6e270081513b36e220bc3817a1e0622f950
Lukie Kang <lukiek+f@gmail.com>
2019-06-30 17:12:54 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -4,12 +4,9 @@ class User
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
-    @knowledge = []
   end
 
-  def knowledge
-    @knowledge
-  end
+
 
 
 
